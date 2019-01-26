54b0ade083316955db7ea1e509120a5e00884dba
Maryyam Niazi <niazim3@mcmaster.ca>
2019-01-26 15:12:40 -0500

    added implementation for checking platform and requesting record permission; needs testing

--- Appnea/src/app/mic-impl/mic-impl.component.ts
+++ Appnea/src/app/mic-impl/mic-impl.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TNSRecorder } from 'nativescript-audio';
 
 @Component({
   selector: 'ns-mic-impl',
@@ -8,7 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MicImplComponent implements OnInit {
 
-  constructor() { }
+	private __recorder: TNSRecorder;
+
+	constructor() {
+
+		isAndroid = false;
+
+		// check platform
+		if (application.android) { 
+			console.log("We are running on Android device!");
+			isAndroid = true;
+		} 
+		else if (application.ios) {
+			console.log("We are running on iOS device");
+		}
+
+		this.__recorder = new TNSRecorder();
+	  	this.__recorder.debug = true; // set true to enable TNSRecorder console logs for debugging
+
+	  	// need explicit permission from user to allow recording
+	  	if isAndroid
+	  	{
+	  		// ask for recording permission until granted
+	  		while (!this.__recorder.hasRecordPermission())
+	  		{
+	  			this.__recorder.requestRecordPermission();
+
+	  			if this.__recorder.hasRecordPermission()
+	  			{
+	  				console.log("You gave us permission to record.");
+		  		}
+		  		else
+		  		{
+		  			console.log("We need your permission to record audio in order to record audio.");
+		  		}
+	  		}
+	  	}
+
+	  	this.__recorder.start();
+	  	
+  }
 
   ngOnInit() {
   }
