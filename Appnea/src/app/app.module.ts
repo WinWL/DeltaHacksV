9aec5f832a44ffc816cd383712bc699073308ed6
Maryyam Niazi <niazim3@mcmaster.ca>
2019-01-26 14:03:01 -0500

    created component for microphone implementation

--- Appnea/src/app/app.module.ts
+++ Appnea/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
+import { MicImplCompComponent } from './microphone-mod/mic-impl-comp/mic-impl-comp.component';
+import { MicImplComponent } from './mic-impl/mic-impl.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -23,7 +25,9 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     declarations: [
         AppComponent,
         ItemsComponent,
-        ItemDetailComponent
+        ItemDetailComponent,
+        MicImplCompComponent,
+        MicImplComponent
     ],
     providers: [],
     schemas: [
---
+++ Appnea/src/app/mic-impl/mic-impl.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-mic-impl',
+  templateUrl: './mic-impl.component.html',
+  styleUrls: ['./mic-impl.component.css'],
+  moduleId: module.id,
+})
+export class MicImplComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
