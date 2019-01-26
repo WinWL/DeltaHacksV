2a6bb7c58984ae3f266e38bb96f66d9290ee6514
Maryyam Niazi <niazim3@mcmaster.ca>
2019-01-26 17:38:52 -0500

    added start component

---
+++ Appnea/src/app/start/start.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-start',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.css'],
+  moduleId: module.id,
+})
+export class StartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
