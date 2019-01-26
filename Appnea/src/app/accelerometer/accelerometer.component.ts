2734180b4492792e31e5bc0e8c78320599cade50
Winfield Lai <laiw3@mcmaster.ca>
2019-01-26 16:06:19 -0500

    Working Accelerometer
    Why:
    
    This change addresses the need by:

--- Appnea/src/app/accelerometer/accelerometer.component.ts
+++ Appnea/src/app/accelerometer/accelerometer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as accelerometer from 'nativescript-accelerometer'; // Requiring the plugin module 
 
 @Component({
   selector: 'ns-accelerometer',
@@ -8,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccelerometerComponent implements OnInit {
 
+  display: string;
+
+  xCoor: number;
+  yCoor: number;
+  zCoor: number;
+
+
+  sensorDelay = 'ui';
+
   constructor() { }
 
   ngOnInit() {
+
+
+    accelerometer.startAccelerometerUpdates(function(data) {
+      console.log("x: " + data.x + "y: " + data.y + "z: " + data.z);
+
+      this.xCoor = data.x;
+      this.yCoor = data.y;
+      this.zCoor = data.z;
+
+  }, { sensorDelay: "ui" });
+
+    this.display = 'Hello world'; 
+
   }
+
+
+
 
 }
