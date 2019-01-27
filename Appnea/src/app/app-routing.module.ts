8b8a7871eba4db16e57489aa793e59a94492cc18
Maryyam Niazi <niazim3@mcmaster.ca>
2019-01-26 21:29:29 -0500

    added routes to saveFile and commFile components

--- Appnea/src/app/app-routing.module.ts
+++ Appnea/src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { Routes } from "@angular/router";
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
 import { AccelerometerComponent } from './accelerometer/accelerometer.component';
+import { CommFileComponent } from './comm-file/comm-file.component';
+import { SaveFileComponent } from './save-file/save-file.component';
 
 const routes: Routes = [
     { path: "", redirectTo: "/items", pathMatch: "full" },
     { path: "accelerometer", component: AccelerometerComponent },
     { path: "items", component: ItemsComponent },
     { path: "item/:id", component: ItemDetailComponent },
+    { path: "commService", component: CommFileComponent },
+    { path: "saveFile", component: SaveFileComponent }
 ];
 
 @NgModule({
