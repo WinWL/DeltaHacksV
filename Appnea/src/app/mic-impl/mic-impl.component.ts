1a91fd4ac26e077712690f43a0998ef486da6972
Winfield Lai <laiw3@mcmaster.ca>
2019-01-27 12:14:34 -0500

    Commit

--- Appnea/src/app/mic-impl/mic-impl.component.ts
+++ Appnea/src/app/mic-impl/mic-impl.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TNSRecorder } from 'nativescript-audio';
 import { knownFolders, Folder, File } from "tns-core-modules/file-system";
+import { Observable } from "tns-core-modules/data/observable";
+import * as Permissions from "nativescript-permissions";
+declare var android: any;
+import { android as androidApp, ios as iosApp } from "tns-core-modules/application";
 
 
 @Component({
@@ -12,6 +16,8 @@ import { knownFolders, Folder, File } from "tns-core-modules/file-system";
 export class MicImplComponent implements OnInit {
 
   micRecorder:TNSRecorder;
+  vm = new Observable();
+
 
   constructor() { }
 
@@ -19,16 +25,72 @@ export class MicImplComponent implements OnInit {
     console.log(TNSRecorder.CAN_RECORD());
 
    this.micRecorder = new TNSRecorder();
+
   }
 
+  make() {
+    if (androidApp) {
+      console.log("We are running on Android device!");
+      
+    }
 
+    const documents: Folder = knownFolders.documents();
+    const folder: Folder = documents.getFolder("TestFolder");
+    const file: File = folder.getFile(`test` + `.txt`);
+
+    console.log("Make", folder, file );
+
+    file.writeText('Hello')
+        .then(() => {
+            file.readText()
+                .then((res) => {
+                    console.log(res);
+                });
+        }).catch((err) => {
+            console.log(err);
+      });
+
+  }
 
   start(){
     console.log("Start")
-    let aOption: AudioRecorderOptions = new AudioRecorderOptions();
-      aOption.filename = "test.wav"
 
-    this.micRecorder.start(aOption)
+    const documents: Folder = knownFolders.documents();
+    const folder: Folder = documents.getFolder("TestFolder");
+    const file: File = documents.getFile(`microphone` + `.wav`);  
+
+    console.log(file.path)
+    
+    this.micRecorder.requestRecordPermission().then(() => {
+      if (TNSRecorder.CAN_RECORD()) {
+  
+        let recorder = new TNSRecorder();
+        let data = new Observable()
+
+        var recorderOptions = {
+    
+          filename: file.path,
+          infoCallback: function () {
+            console.log('infoCallback');
+          },
+          errorCallback: function () {
+            console.log('errorCallback');
+            alert('Error recording.');
+          }
+        };
+    
+      console.log('RECORDER OPTIONS: ' + recorderOptions);
+      recorder.start(recorderOptions).then(function (res) {
+          data.set('isRecording', true);
+      }, function (err) { 
+          data.set('isRecording', false);
+          console.log('ERROR: ' + err);
+      })
+      }
+
+     // this.micRecorder.start(recorderOptions)
+    })
+    
   }
 
   stop(){
